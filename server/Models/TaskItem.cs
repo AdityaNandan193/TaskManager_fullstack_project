b4a159bcb7bb876de66fd898e76e55dcf4b352a2
Aditya <adityanandan659@gmail.com>
2025-04-08 15:59:13 +0530

    code upload

---
+++ server/Models/TaskItem.cs
@@ -0,0 +1,11 @@
+namespace server.Models
+{
+    public class TaskItem
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public bool IsCompleted { get; set; }
+        public string UserId { get; set; } = string.Empty;
+    }
+}
---
+++ server/Program.cs
@@ -0,0 +1,31 @@
+using server.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite("Data Source=taskdb.db"));
+
+
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+        policy.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
+});
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+app.UseCors("AllowAll");
+
+app.UseSwagger();
+app.UseSwaggerUI();  
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
