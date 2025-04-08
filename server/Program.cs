3345b0bddd14adffa5e6f023c2237a071ee1ad86
Aditya Nandan <115461905+AdityaNandan193@users.noreply.github.com>
2025-04-08 19:18:26 +0530

    Update Program.cs

--- server/Program.cs
+++ server/Program.cs
@@ -6,12 +6,12 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlite("Data Source=taskdb.db"));
 
-
-
 builder.Services.AddCors(options =>
 {
-    options.AddPolicy("AllowAll", policy =>
-        policy.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
+    options.AddPolicy("AllowFrontend", policy =>
+        policy.WithOrigins("https://task-manager-frontend-bay-seven.vercel.app")
+              .AllowAnyHeader()
+              .AllowAnyMethod());
 });
 
 builder.Services.AddControllers();
@@ -20,12 +20,13 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-app.UseCors("AllowAll");
+app.UseCors("AllowFrontend");
 
 app.UseSwagger();
-app.UseSwaggerUI();  
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
+
 app.Run();
